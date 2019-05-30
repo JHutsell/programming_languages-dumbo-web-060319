0d9fdacab599ae4ae798cd3d5cc3f66a82e8c198
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 22:26:38 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,4 +1,15 @@
+
 def reformat_languages(languages)
-  # your code here
-  languages.each do 
+  hash = {}
+    languages.each do |style, data|
+      data.each do |lang, type|
+        if hash.has_key?(lang)
+          hash[lang][:style] << style
+        else
+          hash[lang] = type
+          hash[lang][:style] = [style]
+        end
+      end
+    end
+  hash
 end
